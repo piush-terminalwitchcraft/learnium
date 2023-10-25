8f4b14247f5dfaced0a19696591c56d84afe4d10
piush-terminalwitchcraft <piushpaul.16@gmail.com>
2023-10-26 01:59:49 +0530

    Initial code completed🚀

---
+++ Components/Elements/index.js
@@ -0,0 +1,3 @@
+export {default as Button} from "./Button.tsx";
+export {default as CustomInput} from "./CustomInput.tsx";
+export {default as Card} from "./Card.tsx";
---
+++ Utils/Constants.ts
@@ -0,0 +1,24 @@
+import { AdminFeature } from "./Types";
+
+export const ADMIN_FEATURES: AdminFeature[] = [
+  {
+    "name":"Articles", 
+    "image": '/articles.png',
+    "link": '/Admin/Articles'
+  }, 
+  {
+    "name":"Classroom", 
+    "image": '/classroom.png',
+    "link": '/Admin/Classroom'
+  }, 
+  {
+    "name":"Profile",
+    "image": '/profile_photo.png',
+    "link": '/Admin/Profle'
+  }, 
+  {
+    "name":"Access",
+    "image": '/admin_access.png',
+    "link": '/Admin/Login'
+  },
+]
